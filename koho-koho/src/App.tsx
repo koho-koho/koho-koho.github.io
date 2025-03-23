fe2c6729a03f020cea494b3cecd41d6aa1df2750
Milindi_Kodikara <milindikodikara@gmail.com>
2025-03-23 13:20:00 +1100

    Update descriptions and padding

--- koho-koho/src/App.tsx
+++ koho-koho/src/App.tsx
@@ -13,17 +13,24 @@ const App = () => {
                     <img className="App-logo heading-container" src={logo} alt="logo"/>
                 </div>
                 <div className="description-container">
-                    <p> 🌾 මෙම වෙබ් අඩවියේ ප්‍රධාන අරමුණ වන්නේ ඔබේ වේලා කලාපය මත පදනම්ව සිංහල සහ දෙමළ අලුත් අවුරුදු
+                    <p> 🌾 මෙම වෙබ් අඩවියේ ප්‍රධාන අරමුණ වන්නේ ශ්‍රී ලංකාවේ නැකැත් වේලාවට සමගාමීව ඔබේ වේලා කලාපය මත 2025 වසරේ සිංහල සහ දෙමළ අලුත් අවුරුදු
                         නැකැත් වේලාවන් ප්‍රදර්ශනය කිරීමයි.</p>
 
-                    <p> 🌾 This website's main purpose is to showcase the auspicious times for the Sinhala and Tamil New
-                        Year, based on your timezone. </p>
+                    <p> 🌾 This website's main purpose is to showcase the auspicious times for the 2025 Sinhala and Tamil New
+                        Year in Sri Lanka, based on your timezone. </p>
+                </div>
+                <div className="header-container">
+                    <img className="App-logo heading-container" src={logo} alt="logo"/>
+                    <div className="heading-container heading">2025 වසරේ නැකැත් වේලාවන් : : Auspicious Times for the year 2025</div>
+                    <img className="App-logo heading-container" src={logo} alt="logo"/>
                 </div>
                 <div>
                     <Nakath/>
                 </div>
             </header>
-            <footer className="footer">Made with ❤️ by Milindi Kodikara : : <a href="https://ko-fi.com/milindi"> Buy me a tea</a> 🍃</footer>
+            <footer className="footer">Made with ❤️ by Milindi Kodikara : : <a href="https://ko-fi.com/milindi"> Buy me
+                a tea</a> 🍃
+            </footer>
         </div>
     )
 }
